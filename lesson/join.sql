e1317dada5cb86a1d47b250f5e84985960cb784e
{marimo_7} <{220098tt@yse-c.net}>
2023-06-30 10:27:52 +0900

    upload

---
+++ lesson/join.sql
@@ -0,0 +1,50 @@
+-- 2023-06-30 2.week3
+
+-- INNER JOIN (内部結合)
+SELECT
+    *
+FROM user_items
+JOIN users ON user_items.user_id = users.id;
+
+-- user_itemsを基準に usersと結合
+SELECT
+    users.id,
+    users.name,
+    user_items.item_id,
+    user_items.amount,
+    user_items.created_at
+FROM user_items
+INNER JOIN users ON user_items.user_id = users.id;
+
+-- usersを基準に user_itemsと結合
+SELECT
+    users.id,
+    users.name,
+    user_items.item_id,
+    user_items.amount,
+    user_items.created_at
+FROM users
+INNER JOIN user_items ON user_items.user_id = users.id;
+
+--------------------------------------------------------------------------------
+-- LEFT JOIN (外部結合)
+
+-- user_itemsを基準に usersと結合(LEFT)
+SELECT
+    users.id,
+    users.name,
+    user_items.item_id,
+    user_items.amount,
+    user_items.created_at
+FROM user_items
+LEFT JOIN users ON user_items.user_id = users.id;
+
+-- usersを基準に user_itemsと結合(LEFT)
+SELECT
+    users.id,
+    users.name,
+    user_items.item_id,
+    user_items.amount,
+    user_items.created_at
+FROM users
+LEFT JOIN user_items ON user_items.user_id = users.id;
---
+++ lesson/user_items_data.sql
@@ -0,0 +1,13 @@
+-- 2023-06-30 2.week3
+
+-- INSERT INTO items (code, name, price, stock) 
+--     VALUES ("D00001", "コーヒー", "300");
+-- INSERT INTO items (code, name, price, stock) 
+--     VALUES ("D00002", "紅茶", "300");
+-- INSERT INTO items (code, name, price, stock) 
+--     VALUES ("D00003", "ほうじ茶", "300");
+
+INSERT INTO user_items (user_id, item_id, amount, total_price) VALUES (1, 1, 3, 900);
+INSERT INTO user_items (user_id, item_id, amount, total_price) VALUES (2, 1, 1, 300);
+INSERT INTO user_items (user_id, item_id, amount, total_price) VALUES (1, 3, 2, 800);
+INSERT INTO user_items (user_id, item_id, amount, total_price) VALUES (3, 2, 2, 700);
