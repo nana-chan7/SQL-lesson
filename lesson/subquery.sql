11b0507c466eb1b4ad058ca1c4fa56030ddbbf00
{marimo_7} <{220098tt@yse-c.net}>
2023-06-30 14:17:38 +0900

    up

--- lesson/subquery.sql
+++ lesson/subquery.sql
@@ -1,5 +1,6 @@
 -- gender = female の購入履歴
 -- Subqueryの場合
+-- WHEREのサブクエリ
 SELECT * FROM user_items
 WHERE user_items.user_id IN (SELECT id FROM users WHERE gender = "female");
 
@@ -13,8 +14,29 @@ FROM user_items
 JOIN users ON user_items.user_id = users.id
 WHERE user_items.user_id IN (SELECT id FROM users WHERE gender = "female");
 
-
 -- JOINの場合
 SELECT * FROM user_items
 JOIN users ON user_items.user_id = user_id
 WHERE users.gender = "female"; 
+
+-- 平均価格より高い商品
+SELECT * FROM items WHERE price >= (SELECT AVG(price) FROM items);
+-------------------------------------------------------------------------------
+-- FROMのサブクエリ
+SELECT
+    male_users.name,
+    male_users.gender,
+    user_items.*
+FROM (SELECT * FROM users WHERE gender = "male") AS male_users
+JOIN user_items ON male_users.id = user_items.user_id;
+
+-- JOINの場合
+SELECT
+    users.name,
+    users.gender,
+    user_items.*
+FROM users
+JOIN user_items ON users.id = user_items.user_id
+WHERE users.gender = "male";
+
+
